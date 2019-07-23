ed1df02e291184ebff48438c52e9565c9196ba5f
Ray van Toll <48050007+rayvtoll@users.noreply.github.com>
2019-07-23 10:09:50 +0200

    Already in base-image so not needed here anymore

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-chown -R $USER:$USER /home/$USER/
-useradd -m $USER ; echo "$USER:$USER" | chpasswd
-mkdir -p /home/$USER/.ssh
-echo $KEY > /home/$USER/.ssh/authorized_keys
-chown -R $USER:$USER /home/$USER/
-sh -c /usr/sbin/sshd && tail -f /dev/null
